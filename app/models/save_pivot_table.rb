2576b8a70415c2a2bf7d2e2b28e6d95785a55118
admin <admin@example.net>
2018-05-15 17:54:49 +0100

    fix

--- app/models/save_pivot_table.rb
+++ app/models/save_pivot_table.rb
@@ -1,4 +1,6 @@
 class SavePivotTable < ApplicationRecord
   validates_presence_of  :name, :report_id
   validates_uniqueness_of  :name, scope: [:report_id]
+
+  belongs_to :user, optional: true
 end
