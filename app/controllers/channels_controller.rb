7a7d7fc52292e2d3b7d11866d58dfd76860cc8ba
admin <admin@example.net>
2018-06-26 21:30:12 +0100

    fix report visibility

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -23,7 +23,8 @@ class ChannelsController < ApplicationController
   # GET /channels/1
   # GET /channels/1.json
   def show
-    @reports = @channel.shared_report? ? User.current.reports : @channel.visible_reports
+    reports = @channel.shared_report? ? User.current.reports : @channel.visible_reports
+    @reports = reports.where(channel_id: Channel.pluck(:id))
   end
 
   # GET /channels/new
