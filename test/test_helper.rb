5f546eaa6cefb4133a8e0a516a7e808af2f5a552
bilel-kedidi <bilel.kedidi@gmail.com>
2019-01-09 14:07:21 +0100

    add database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,18 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'database_cleaner'
+
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  
   fixtures :all
+  ActiveRecord::Migration.check_pending!
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.logger = Rails.logger
+  setup { DatabaseCleaner.start }
+  teardown { DatabaseCleaner.clean }
 
   # Add more helper methods to be used by all tests here...
 end
