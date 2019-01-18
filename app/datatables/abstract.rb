f93957c8f5313e7a76a7032ef94ddc0e59b8cc36
bilel-kedidi <bilel.kedidi@gmail.com>
2019-01-18 19:15:49 +0100

    fix dattable

--- app/datatables/abstract.rb
+++ app/datatables/abstract.rb
@@ -6,4 +6,21 @@ class Abstract < AjaxDatatablesRails::Base
     end
     conditions.compact.reduce(:and)
   end
+
+  def sort_records(records)
+    sort_by = []
+    params[:order].values.each do |item|
+      sort_by << "#{sort_column(item)} #{sort_direction(item)}"
+    end
+    records.order(sort_by.join(", "))
+  end
+
+  def generate_sortable_displayed_columns
+    @sortable_displayed_columns = []
+    params[:columns].values.each do |column|
+      @sortable_displayed_columns << column[:data] if column[:orderable] == 'true'
+    end
+    @sortable_displayed_columns
+  end
+
 end
