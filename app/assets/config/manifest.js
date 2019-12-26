1548d274023dbae2de29d2b8fd871aabe7693b64
bilel-kedidi <bilel.kedidi@gmail.com>
2019-12-26 10:36:53 +0100

    add js to manifest + fix font size in grid

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,19 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link pivottables/smart_admin.js
+//= link pivottables/highstock.js
+//= link pivottables/highstock.js
+//= link pivottables/highcharts-more.js
+//= link pivottables/treemap.js
+
+//= link pivottables/pivot.js
+//= link pivottables/d3.min.js
+//= link pivottables/jquery.ui.touch-punch.min.js
+//= link pivottables/c3.min.js
+//= link pivottables/c3_renderers.min.js
+//= link pivottables/plotly-basic-latest.min.js
+//= link pivottables/plotly_renderers.js
+
+//= link pivottables/subtotal.min.js
+
+//= link pivottables/handsontable.full.min.js
