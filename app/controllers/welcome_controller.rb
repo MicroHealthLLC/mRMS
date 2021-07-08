f8cbf484081da597bee81ff7c43da53b9c80690e
b-solution <bilel.kedidi@gmail.com>
2021-07-08 23:51:20 +0500

    Show More link should only display when there are more reports on Home Page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,10 @@ class WelcomeController < ApplicationController
     @personal_reports = params[:display_all_personal].present? ? Report.personal_channel_reports : Report.personal_channel_reports.limit(3)
     @group_reports = params[:display_all_group].present? ? Report.group_channel_reports : Report.group_channel_reports.limit(3)
 
+    @public_reports_count = Report.public_channel_reports.count
+    @personal_reports_count = Report.personal_channel_reports.count
+    @group_reports_count = Report.group_channel_reports.count
+
     session[:appointment_store_id] = nil if User.current_user.can?(:manage_roles)
     if session[:employee_id]
       session[:appointment_store_id] = nil
