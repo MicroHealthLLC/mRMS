3e59114cdb545f93a0b31c61a7ff68f25b5efbf9
b-solution <bilel.kedidi@gmail.com>
2021-07-19 17:41:42 +0500

    Fixed MultiDataSetDashboard name field must be required

--- app/models/multi_data_set_dashboard.rb
+++ app/models/multi_data_set_dashboard.rb
@@ -1,6 +1,7 @@
 class MultiDataSetDashboard < ApplicationRecord
   belongs_to :channel
   belongs_to :user
+  validates_presence_of :name
 
   has_many :shared_multi_report_dashboards, dependent: :destroy
   has_many :save_pivot_tables, through: :shared_multi_report_dashboards
