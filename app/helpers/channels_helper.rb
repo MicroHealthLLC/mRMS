8cb43f8bf7b030c801f15dbe1358dd597e471de1
b-solution <bilel.kedidi@gmail.com>
2021-07-19 16:36:50 +0500

    Fixed button disp

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,8 @@
 module ChannelsHelper
+
+	def pivot_table_reports?(channel)
+		report_ids = channel.reports.pluck(:id)
+		pivot_table = SavePivotTable.where(report_id: report_ids)
+		return !pivot_table.present?
+	end
 end
