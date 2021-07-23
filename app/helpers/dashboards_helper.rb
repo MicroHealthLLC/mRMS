b2504fe361137e91f4284497fa3d9ad2e366d526
b-solution <bilel.kedidi@gmail.com>
2021-07-23 10:45:56 +0500

    fixed should retain reports drag n drop order within reports box when fa…

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,11 @@
 module DashboardsHelper
+
+def pluck_pivot_table(dashboard)
+  pluckIds = []
+  dashboard.save_pivot_tables.each do|pivot|
+    pluckIds.push(pivot.id)
+  end
+  return pluckIds
+end
+
 end
