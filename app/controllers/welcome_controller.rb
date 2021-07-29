4adb8bfcce6b93afd10cd8a40ec27099e6930f29
b-solution <bilel.kedidi@gmail.com>
2021-07-29 14:20:50 +0500

    Include Multi Data Set Dashboards section into home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -31,6 +31,7 @@ class WelcomeController < ApplicationController
     @personal_dashboard_count = personal_dashboard.count
     @group_dashboard_count = group_dashboard_reports.count
 
+    load_multi_shared_dashboard_data
     session[:appointment_store_id] = nil if User.current_user.can?(:manage_roles)
     if session[:employee_id]
       session[:appointment_store_id] = nil
@@ -43,5 +44,37 @@ class WelcomeController < ApplicationController
   end
 
   def onedriveredirect
+  end
+
+  private
+  def load_multi_shared_dashboard_data
+    public_multi_dashboards          = MultiDataSetDashboard.public_channel_multi_dashboard
+    personal_multi_dashboards        = MultiDataSetDashboard.personal_channel_multi_dashboard
+    group_multi_dashboards           = MultiDataSetDashboard.group_channel_multi_dashboard
+
+    public_shared_multi              = SharedMultiReportDashboard.where(multi_data_set_dashboard_id: public_multi_dashboards.pluck(:id)).pluck(:pivot_table_id)
+    personal_shared_multi            = SharedMultiReportDashboard.where(multi_data_set_dashboard_id: personal_multi_dashboards.pluck(:id)).pluck(:pivot_table_id)
+    group_shared_multi               = SharedMultiReportDashboard.where(multi_data_set_dashboard_id: group_multi_dashboards.pluck(:id)).pluck(:pivot_table_id)
+
+    public_shared_pivot_tables       = SavePivotTable.where(id: public_shared_multi)
+    personal_shared_pivot_tables     = SavePivotTable.where(id: personal_shared_multi)
+    group_shared_pivot_tables        = SavePivotTable.where(id: group_shared_multi)
+
+    @public_shared_reports           = params[:display_all_public_shared].present? ? public_shared_pivot_tables : public_shared_pivot_tables.first(3)
+    @personal_shared_reports         = params[:display_all_personal_shared].present? ? personal_shared_pivot_tables : personal_shared_pivot_tables.first(3)
+    @group_shared_reports            = params[:display_all_group_shared].present? ? group_shared_pivot_tables : group_shared_pivot_tables.first(3)
+
+    @public_shared_dashboard         = params[:display_all_public_shared_dashboard].present? ? public_multi_dashboards : public_multi_dashboards.first(3)
+    @personal_shared_dashboard       = params[:display_all_personal_shared_dashboard].present? ? personal_multi_dashboards : personal_multi_dashboards.first(3)
+    @group_shared_dashboard          = params[:display_all_group_shared_dashboard].present? ? group_multi_dashboards : group_multi_dashboards.first(3)
+
+    @public_shared_reports_count     = public_shared_pivot_tables.count
+    @personal_shared_reports_count   = personal_shared_pivot_tables.count
+    @group_shared_reports_count      = group_shared_pivot_tables.count
+
+    @public_shared_dashboard_count   = public_multi_dashboards.count
+    @personal_shared_dashboard_count = personal_multi_dashboards.count
+    @group_dashboard_shared_count    = group_multi_dashboards.count
+
   end
 end
