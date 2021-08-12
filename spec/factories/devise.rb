6021f0851aaffe788137fc79b65a0fc5063cd77b
b-solution <bilel.kedidi@gmail.com>
2021-08-12 11:32:10 +0500

    Rspec integration

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,18 +1,17 @@
 require 'faker'
 
 FactoryBot.define do
-
   factory :admin do
     email {"admin@gmail.com"}
-    password {Faker::Internet.password}
+    password {"Admin@2018"}
     admin = true
     state = :active
     login { Faker::Name.first_name }
   end
 
   factory :user do
-    email {"admin@gmail.com"}
-    password {"Admin@2018"}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password}
     admin = true
     state = :active
     login { Faker::Name.first_name }
