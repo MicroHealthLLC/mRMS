292cf2469e7028946e1b90794f0b4b81f08bff7f
b-solution <bilel.kedidi@gmail.com>
2021-08-17 15:06:14 +0500

    Fixed reports screens display error message.

--- db/migrate/20210816111022_change_default_to_uncategorized_enum.rb
+++ db/migrate/20210816111022_change_default_to_uncategorized_enum.rb
@@ -1,19 +1,9 @@
 class ChangeDefaultToUncategorizedEnum < ActiveRecord::Migration[5.2]
   def change
-    ReportEnum.all.each do|report_enum|
-      if report_enum.name.capitalize == "Default"
-        report_enum.name        = "Uncategorized"
-        report_enum.is_default  = true
-        report_enum.save
-      end
-    end
+    ReportEnum.find_by_name("Default")&.destroy!
+    DashboardEnum.find_by_name("Default")&.destroy!
 
-    DashboardEnum.all.each do|dashbord_enum|
-      if dashbord_enum.name.capitalize == "Default"
-        dashbord_enum.name        = "Uncategorized"
-        dashbord_enum.is_default  = true
-        dashbord_enum.save
-      end
-    end
+    ReportEnum.find_or_create_by(name: 'Uncategorized', active: true, is_default: true)
+    DashboardEnum.find_or_create_by(name: 'Uncategorized', active: true, is_default: true)
   end
 end
