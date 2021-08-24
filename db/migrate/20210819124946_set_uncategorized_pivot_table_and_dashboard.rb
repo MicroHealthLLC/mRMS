fac2ee1b6cd46b3df245fe32c3e990b4e1a0e7ba
b-solution <bilel.kedidi@gmail.com>
2021-08-24 20:27:56 +0500

    Fixed default value to uncategorized

--- db/migrate/20210819124946_set_uncategorized_pivot_table_and_dashboard.rb
+++ db/migrate/20210819124946_set_uncategorized_pivot_table_and_dashboard.rb
@@ -1,13 +1,13 @@
-class SetUncategorizedPivotTable < ActiveRecord::Migration[5.2]
+class SetUncategorizedPivotTableAndDashboard < ActiveRecord::Migration[5.2]
   def change
     report_enum     = ReportEnum.find_or_create_by(name: 'Uncategorized')
     dashboard_enum  = DashboardEnum.find_or_create_by(name: 'Uncategorized')
 
-    SavePivotTable.where(report_enum_id: nil).each do|pivot|
+    SavePivotTable.where(report_enum_id: [nil, ""]).each do|pivot|
       pivot.update(report_enum_id: report_enum.id)
     end
 
-    Dashboard.where(dashboard_enum_id: nil).each do|dashboard|
+    Dashboard.where(dashboard_enum_id: [nil, ""]).each do|dashboard|
       dashboard.update(dashboard_enum_id: dashboard_enum.id)
     end
   end
