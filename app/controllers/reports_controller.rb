7bc6dbe4a30d2724354b569ef60607aaca1df5ec
b-solution <bilel.kedidi@gmail.com>
2021-08-24 15:16:08 +0500

    Selecting a Filter option on full screen mode should display result on full screen mode

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,9 +9,12 @@ class ReportsController < ApplicationController
   # GET /reports/1
   # GET /reports/1.json
   def show
+
     @query_id         = params[:query_id] rescue nil
     @pivot_tables     = params[:report_enum_id] ? @report.save_pivot_tables.where(report_enum_id: params[:report_enum_id]) : @report.save_pivot_tables
     @report_dashboard = params[:dashboard_enum_id] ? @report.dashboards.where(dashboard_enum_id: params[:dashboard_enum_id]) : @report.dashboards
+    @report_enum_id   = params[:report_enum_id]
+    @dashboard_enum_id = params[:dashboard_enum_id]
     @dashboard_count = @report.dashboards.count
     render_403 unless  @channel.is_public? or @channel.is_creator? or @channel.my_permission.can_view_report?
     # if @report.document_url
