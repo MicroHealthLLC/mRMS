3238ba636e2e141167b39d4cf10c6d2a54e4acee
b-solution <bilel.kedidi@gmail.com>
2021-08-26 17:02:18 +0500

    fixed the order of channels in sidebar and modify home screen display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,48 +27,27 @@ class WelcomeController < ApplicationController
   private
 
   def load_multi_shared_dashboard_data
-    disp_more = params[:display_more]
-    public_multi_dashboards          = MultiDataSetDashboard.public_channel_multi_dashboard
-    personal_multi_dashboards        = MultiDataSetDashboard.personal_channel_multi_dashboard
-    group_multi_dashboards           = MultiDataSetDashboard.group_channel_multi_dashboard
-
-    @public_shared_dashboard         = (disp_more == "display_all_public_shared_dashboard") ? public_multi_dashboards : public_multi_dashboards.first(3)
-    @personal_shared_dashboard       = (disp_more == "display_all_personal_shared_dashboard") ? personal_multi_dashboards : personal_multi_dashboards.first(3)
-    @group_shared_dashboard          = (disp_more == "display_all_group_shared_dashboard") ? group_multi_dashboards : group_multi_dashboards.first(3)
-
-    @public_shared_dashboard_count   = public_multi_dashboards.count
-    @personal_shared_dashboard_count = personal_multi_dashboards.count
-    @group_dashboard_shared_count    = group_multi_dashboards.count
+    public_multi_dashboards = MultiDataSetDashboard.public_channel_multi_dashboard
+    personal_multi_dashboards = MultiDataSetDashboard.personal_channel_multi_dashboard
+    group_multi_dashboards = MultiDataSetDashboard.group_channel_multi_dashboard
+    shared_dashboard_all = public_multi_dashboards + personal_multi_dashboards + group_multi_dashboards
+    @shared_dashboard_all = shared_dashboard_all.first(3)
   end
 
   def load_pivot_tables
     public_pivot_tables = SavePivotTable.where(report_id: @public_report_ids)
     personal_pivot_tables = SavePivotTable.where(report_id: @personal_report_ids)
     group_pivot_tables = SavePivotTable.where(report_id: @group_report_ids)
-
-    disp_more = params[:display_more]
-
-    @public_reports = (disp_more == "display_all_public") ? public_pivot_tables : public_pivot_tables.first(3)
-    @personal_reports = (disp_more == "display_all_personal") ? personal_pivot_tables : personal_pivot_tables.first(3)
-    @group_reports = (disp_more == "display_all_group") ? group_pivot_tables : group_pivot_tables.first(3)
-
-    @public_reports_count =  public_pivot_tables.count
-    @personal_reports_count = personal_pivot_tables.count
-    @group_reports_count = group_pivot_tables.count
+    all_pivot_tables = public_pivot_tables.merge(personal_pivot_tables.merge(group_pivot_tables))
+    @all_reports = all_pivot_tables.order('frequently_count desc').first(6)
   end
 
   def load_dashboards
-    disp_more = params[:display_more]
     public_dashboard = Dashboard.where(report_id: @public_report_ids)
     personal_dashboard = Dashboard.where(report_id: @personal_report_ids)
-    group_dashboard_reports = Dashboard.where(report_id: @group_report_ids)
-    @public_dashboard = (disp_more == "display_all_public_dashboard") ? public_dashboard : public_dashboard.first(3)
-    @personal_dashboard = (disp_more == "display_all_personal_dashboard") ? personal_dashboard : personal_dashboard.first(3)
-    @group_dashboard = (disp_more == "display_all_group_dashboard") ? group_dashboard_reports : group_dashboard_reports.first(3)
-
-    @public_dashboard_count = public_dashboard.count
-    @personal_dashboard_count = personal_dashboard.count
-    @group_dashboard_count = group_dashboard_reports.count
+    group_dashboard = Dashboard.where(report_id: @group_report_ids)
+    dashboard_all = public_dashboard.merge(personal_dashboard.merge(group_dashboard))
+    @dashboard_all = dashboard_all.order('frequently_count desc').first(3)
   end
 
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,5 @@
 module WelcomeHelper
+  def check_root?
+    request.env['PATH_INFO'] == root_path
+  end
 end
