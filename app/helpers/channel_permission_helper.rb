ee8036c1229d6f7057a7f876bb420a8621a6a512
b-solution <bilel.kedidi@gmail.com>
2021-09-02 19:30:05 +0500

    Owner on first row of Channel Permissions and Previous Owner Permissions Retained

--- app/helpers/channel_permission_helper.rb
+++ app/helpers/channel_permission_helper.rb
@@ -1,2 +1,13 @@
 module ChannelPermissionHelper
+
+  def permission_sort(permissions,channel)
+    permissions_data = permissions
+    if (permissions.count > 1)
+      owner = []
+      data = permissions.where.not(user_id: channel.user_id)
+      owner << permissions.find_by(user_id: channel.user_id)
+      permissions_data = owner.concat(data)
+    end
+    permissions_data
+  end
 end
