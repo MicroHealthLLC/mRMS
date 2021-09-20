236e88e9e8447a99fab4afe94702a9eca1bdb978
b-solution <bilel.kedidi@gmail.com>
2021-09-20 20:14:06 +0500

    Fixed drive issue to refresh data

--- app/services/one_drive_refresh_service.rb
+++ app/services/one_drive_refresh_service.rb
@@ -16,24 +16,21 @@ class OneDriveRefreshService
 
   def get_one_drive_data
     item_id = @report_document.onedrive_item_id || nil
-    drive_id = item_id.split('!')[0] || nil
-    if drive_id.present? && item_id.present?
-      url = "https://graph.microsoft.com/v1.0/drives/#{drive_id}/items/#{item_id}"
-      items = JSON.parse(HTTParty.get(url,headers: set_headers).body)
-      file_url = items["@microsoft.graph.downloadUrl"] || nil
-      file_name = items["name"] || nil
+    puts "item_id: #{item_id}"
+    url = "https://graph.microsoft.com/v1.0/me/drive/items/#{item_id}" if item_id.present?
+    items = JSON.parse(HTTParty.get(url, headers: set_headers).body)
+    file_url = items["@microsoft.graph.downloadUrl"] || nil
+    file_name = items["name"] || nil
+    puts "file_url: #{file_url}, file_name: #{file_name}"
 
-      if file_url.present? && file_name.present?
-        file_url = items["@microsoft.graph.downloadUrl"]
-        file_name = items["name"]
-        File.open("public/uploads/tmp/#{file_name}", 'wb') do |file|
-          file << open(file_url).read
-          @report_document.file = file
-        end
-        @report_document.save
-      else
-        raise "Access token expired/updated, It is required to re-upload data set!"
+    if file_url.present? && file_name.present?
+      file_url = items["@microsoft.graph.downloadUrl"]
+      file_name = items["name"]
+      File.open("public/uploads/tmp/#{file_name}", 'wb') do |file|
+        file << open(file_url).read
+        @report_document.file = file
       end
+      @report_document.save
     else
       raise "Access token expired/updated, It is required to re-upload data set!"
     end
