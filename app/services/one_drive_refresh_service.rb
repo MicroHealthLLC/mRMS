ecb8c838a4c86678d422ec74c0b2c3660687abcf
b-solution <bilel.kedidi@gmail.com>
2021-09-23 19:27:09 +0500

    Fixed one drive issue

--- app/services/one_drive_refresh_service.rb
+++ app/services/one_drive_refresh_service.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class OneDriveRefreshService
   attr_reader :access_token, :drive_items, :user
 
@@ -32,8 +34,30 @@ class OneDriveRefreshService
       end
       @report_document.save
     else
-      raise "Access token expired/updated, It is required to re-upload data set!"
+      @user.onedrive_refresh_token.present? ? get_new_access_token : reauthorized_onedrive
     end
+  end
+
+  def get_new_access_token
+    options = { :body =>
+      { "client_id" => "#{Setting['AZURE_KEY']}",
+        "redirect_uri" => "#{Setting['CALLBACK_URL']}/auth/microsoft_graph_auth/callback",
+        "client_secret" => "#{Setting['AZURE_SECRET']}",
+        "refresh_token" => "#{@user.onedrive_refresh_token}",
+        "grant_type" => "refresh_token"
+      },
+      :headers => { 'Content-Type' => 'application/x-www-form-urlencoded'}
+    }
+
+    results = HTTParty.post("https://login.live.com/oauth20_token.srf", options)
+    @access_token = results.parsed_response["access_token"]
+    @user.update!(onedrive_access_token: results.parsed_response["access_token"],
+                  onedrive_refresh_token: results.parsed_response["refresh_token"])
+    get_one_drive_data
+  end
+
+  def reauthorized_onedrive
+    open("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=#{Setting['AZURE_KEY']}&scope=#{ENV['AZURE_SCOPES']}&response_type=code&redirect_uri=#{Setting['CALLBACK_URL']}/auth/microsoft_graph_auth/callback")
   end
 
   def set_headers
