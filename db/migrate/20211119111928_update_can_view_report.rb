ab3791f92e273291ffc23841448009d2ce3c896e
b-solution <bilel.kedidi@gmail.com>
2021-11-19 16:41:35 +0500

    All users have Can View Report permission

---
+++ db/migrate/20211119111928_update_can_view_report.rb
@@ -0,0 +1,10 @@
+class UpdateCanViewReport < ActiveRecord::Migration[5.2]
+  def change
+    change_column :channel_permissions, :can_view_report, :boolean, default: true
+
+    channel_permissions = ChannelPermission.all
+    channel_permissions.each do |channel_permission|
+      channel_permission.update(can_view_report: true)
+    end
+  end
+end
